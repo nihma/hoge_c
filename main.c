487fb7c0e7d74741910a9356799c96cd2cfecace
Makoto Nihei <mako3_2h@yahoo.co.jp>
2012-06-29 06:56:35 +0900

    start project

---
+++ main.c
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.c
+ * Author: Makoto Nihei
+ *
+ * Created on 2012/06/29, 6:31
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+  printf("Hello World!\n");
+  return (EXIT_SUCCESS);
+}
+
